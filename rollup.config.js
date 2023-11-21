088631da1d35b93d816c040b3da07943848be033
BusyAPI <contact@busyapi.dev>
2023-11-21 01:30:44 +0100

    Add support for both CJS and ESM

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,20 @@
 import typescript from "rollup-plugin-typescript";
-import { string } from "rollup-plugin-string";
 import del from "rollup-plugin-delete";
 
 export default [
   {
     input: "src/mempass.ts",
-    plugins: [
-      typescript(), // so Rollup can convert TypeScript to JavaScript
-      string({
-        include: "**/*.txt",
-      }),
-      del({ targets: "dist/*" }),
+    plugins: [typescript(), del({ targets: "dist/*" })],
+    output: [
+      {
+        dir: "./dist/esm",
+        external: ["remove-accent"],
+      },
+      {
+        dir: "./dist/cjs",
+        external: ["remove-accent"],
+        format: "cjs",
+      },
     ],
-    output: {
-      dir: "./dist",
-      external: ["remove-accent"],
-    },
   },
 ];
